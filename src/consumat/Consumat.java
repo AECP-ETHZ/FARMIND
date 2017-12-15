22aebaf9103ba42eca2716232f02288c023fbaa0
Kevin Keller <kevin.ew.keller@gmail.com>
2017-12-15 15:30:23 +0100

    Updated consumat to use decision class
    Now each farm returns a decision object with the name of the farm, and the returned products.
    
    The output Batch object takes this decision and formats and output file

--- src/consumat/Consumat.java
+++ src/consumat/Consumat.java
@@ -3,6 +3,7 @@ package consumat;
 import java.util.List;
 
 import agent.farm.Farm;
+import decision.DecisionResult;
 import output.BatchOutput;
 import product.Product;
 import reader.ReadParameters;
@@ -11,33 +12,21 @@ public class Consumat {
 
 	public static void main(String[] args) {
 		
-		// 1 input parameters
 		ReadParameters reader = new ReadParameters();
 		
 		//List<Crop> crops = reader.getCropList();
 		//List<Livestock> livestock = reader.getLivestockList();
 		
-		// 2 create agents
 		List<Farm> farms = reader.getFarms();
 		
 		for ( int i = 0; i < farms.size(); i++) {
 			
-			
-			// update farms with dairydyn simulation results
-			
-			// rerun simulation
-			
-			Product p = farms.get(i).getAction().get(0);
-			
-			System.out.println(String.format("Action: %s", p.getName()  ));	
+			List<Product> p = farms.get(i).getAction();
 
-			System.out.println();
+			DecisionResult decision = new DecisionResult(farms.get(i).getFarmId(), p);
 			
-			// 4 output batch file
-			BatchOutput batch = new BatchOutput(farms.get(i).getFarmName(), p);
+			BatchOutput batch = new BatchOutput(decision);
 			batch.write();
-			
-			// run terminal command (dairydyn)
 		}
 
 	}
--- src/output/BatchOutput.java
+++ src/output/BatchOutput.java
@@ -3,22 +3,28 @@ package output;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
+import decision.DecisionResult;
 import product.Product;
 
 public class BatchOutput implements Output {
-	private String name;
+	private String farmId;
 	private Product p;
 	
 	public BatchOutput(String farmName, Product p) {
-		this.name = farmName;
+		this.farmId = farmName;
 		this.p = p;
+	}
+
+	public BatchOutput(DecisionResult decision) {
+		this.farmId = decision.getFarmId();
+		this.p = decision.getProducts().get(0);
 	}
 
 	public void write() {
 		PrintWriter writer;
 		try {
-			writer = new PrintWriter( String.format("./BatchFiles/%s.txt",this.name ));
-			writer.println(String.format("Farm Name: %s",this.name));
+			writer = new PrintWriter( String.format("./BatchFiles/%s.txt",this.farmId ));
+			writer.println(String.format("Farm ID: %s",this.farmId));
 			writer.println(String.format("Main Product: %s", this.p.getName() ));
 			writer.close();
 			

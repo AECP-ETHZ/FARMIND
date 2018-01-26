fff3cbed6fad99c094c62a05c2c0eb3fa837c851
Kevin Keller <kevin.ew.keller@gmail.com>
2018-01-26 14:02:02 +0100

    added sensitivity parameter selection to main method

--- src/consumat/Consumat.java
+++ src/consumat/Consumat.java
@@ -9,29 +9,27 @@ public class Consumat {
 
 	public static void main(String[] args) {
 		
-		ReadParameters reader = new ReadParameters();
-		List<Farm>     farms = reader.getFarms(100);
-		double[] income = {10000,30000,10000};
-		
-		for (int years = 0; years < 3; years++) {
-			// get updated products and income from farmydyn
-			// farms.updateIncome();
-			// farms.updateProducts();
-
-			// simulate all farms for time period t
-			for ( int i = 0; i < farms.size(); i++) {
-				List<String> p = farms.get(i).getUpdatedActions(farms, income[years]);
-				
-				String id = farms.get(i).getFarmName();
-				System.out.println(id + " " + p.toString());
-
-				farms.get(i).updateExperience();                               // each time period update experience
+		for (int k = 1; k < 2; k++) {															   // sensitivity testing, loop through all parameters
+			ReadParameters reader = new ReadParameters();
+			List<Farm>     farms = reader.getFarms(k);											   // build set of farms with new parameters
+			
+			for (int years = 0; years < 3; years++) {											   // run simulation for a set of years, getting updated income and products
+				// get updated products and income from farmydyn
+				// farms.updateIncome();
+				// farms.updateProducts();
+				double[] income = {10000,30000,10000};
+	
+				for ( int i = 0; i < farms.size(); i++) {                                   	   // simulate all farms for time period t
+					List<String> p = farms.get(i).getUpdatedActions(farms, income[years]);
+					
+					String id = farms.get(i).getFarmName();
+					System.out.println(id + " " + p.toString());
+					// run linear programming model for farm i at this year
+	
+					farms.get(i).updateExperience();                              				   // each time period update experience
+				}
+				System.out.println();
 			}
-			System.out.println();
-
-			// run farm dyn with newly generated batch files for all farms
-			System.out.println();
 		}
-		
 	}
 }

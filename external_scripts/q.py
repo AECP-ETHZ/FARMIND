a291433157cfda3e192e39458a0bf721b8b94215
Kevin Keller <kevin.ew.keller@gmail.com>
2018-04-23 16:39:41 +0200

    updated q .py program

--- external_scripts/q.py
+++ external_scripts/q.py
@@ -3,13 +3,13 @@ experience = range(1,12) # set of years to calculate
 memory = range(3,12) # list of possible years of education
 
 
-ln_ratio = -np.log((1-0.9)/0.9)
+ln_ratio = -np.log((1-0.8)/0.8)
 
-# calculate list of possible k values given memory length and education ratio (s)
+calculate list of possible k values given memory length and education ratio (s)
 def k_func(memory, s, memory_ratio):
 	k = []
 	for m in memory:
-		k.append( float(format( ln_ratio/(m * s * memory_ratio),'.2f' )))
+		k.append( float(format( ln_ratio/( round(m * memory_ratio) * s),'.2f' )))
 	return k
 
 # calculate q value given a k value for all years of experience
@@ -19,33 +19,38 @@ def q_func(k,s):
 		r.append( float(format( 1 / (1 + np.exp(-k*t*s)),'.2f' )))
 	return r
 
-s_list = [0.5,1] # education/max
+s_list = [0.5,1] # Agent_education/Max_education
+for s in s_list:
+	i = 0
+	if s > 0.5:
+		memory_ratio = 0.25
+	else:
+		memory_ratio = 0.5
+	std = []
+	k_list = k_func(memory, s, memory_ratio)	
+	for k in k_list:
+		std.append( float(format(np.std( q_func(k,s) ),'.2f') ) )
+	print("s value: " + str(s) + " and memory ratio: " + str(memory_ratio)) 
+	for k in k_list:
+		print("years of memory: " + str(i+3) + ", years to reach 0.9: " + str( round((i+3)*memory_ratio )) + ", k value: " + str(k) + ", std: " + str(std[i]) + ", q values for years of experience " + str(q_func(k,s)))
+		i += 1
+	print
+
+#ln_ratio = -np.log((1-0.7)/0.7)	
+	
+s_list = [0.5,1] # Agent_education/Max_education
 for s in s_list:
 	i = 0
 	if s > 0.5:
 		memory_ratio = 0.5
 	else:
 		memory_ratio = 1
-
+	std = []
 	k_list = k_func(memory, s, memory_ratio)	
+	for k in k_list:
+		std.append( float(format(np.std( q_func(k,s) ),'.2f') ) )
 	print("s value: " + str(s) + " and memory ratio: " + str(memory_ratio)) 
 	for k in k_list:
-		print("years of memory: " + str(i+3) + ", years to reach 0.9: " + str( (i+3)*memory_ratio ) + ", k value: " + str(k) + ", q values for years of experience " + str(q_func(k,s)))
+		print("years of memory: " + str(i+3) + ", years to reach 0.9: " + str( round((i+3)*memory_ratio )) + ", k value: " + str(k) + ", std: " + str(std[i]) + ", q values for years of experience " + str(q_func(k,s)))
 		i += 1
 	print
-
-
-
-#std = [] # standard deviation of values
-
-#for kk in k:
-#	r = q_func(kk,1)
-#	std.append( float(format(np.std(r),'.2f') ) )
-		
-#i = 0
-#for kk in k:
-	#print "years of experience: " + str(i+3) + ", k value: " + str(kk) + ", std: " + str(std[i])+ " : " + str(q_func(kk,1))
-	#i +=1
-	
-	
-

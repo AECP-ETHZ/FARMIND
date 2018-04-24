6c94395dfbbf75f00a3bf141e0f739f3b309e4a2
Kevin Keller <kevin.ew.keller@gmail.com>
2018-04-24 13:56:25 +0200

    updated q calculation

--- external_scripts/q.py
+++ external_scripts/q.py
@@ -1,11 +1,9 @@
 import numpy as np
 experience = range(1,12) # set of years to calculate
-memory = range(3,12) # list of possible years of education
+memory = range(3,4) # list of possible years of education
+ln_ratio = -np.log((1-0.9)/0.9)
 
-
-ln_ratio = -np.log((1-0.8)/0.8)
-
-calculate list of possible k values given memory length and education ratio (s)
+#calculate list of possible k values given memory length and education ratio (s)
 def k_func(memory, s, memory_ratio):
 	k = []
 	for m in memory:
@@ -19,14 +17,15 @@ def q_func(k,s):
 		r.append( float(format( 1 / (1 + np.exp(-k*t*s)),'.2f' )))
 	return r
 
-s_list = [0.5,1] # Agent_education/Max_education
+ss = range(1,11) # Agent_education/Max_education
+s_list = []
+for i in ss:
+	s_list.append( float(i*1/10.0))
+
 for s in s_list:
 	i = 0
-	if s > 0.5:
-		memory_ratio = 0.25
-	else:
-		memory_ratio = 0.5
 	std = []
+	memory_ratio = 1/6.0
 	k_list = k_func(memory, s, memory_ratio)	
 	for k in k_list:
 		std.append( float(format(np.std( q_func(k,s) ),'.2f') ) )
@@ -36,21 +35,7 @@ for s in s_list:
 		i += 1
 	print
 
-#ln_ratio = -np.log((1-0.7)/0.7)	
 	
-s_list = [0.5,1] # Agent_education/Max_education
-for s in s_list:
-	i = 0
-	if s > 0.5:
-		memory_ratio = 0.5
-	else:
-		memory_ratio = 1
-	std = []
-	k_list = k_func(memory, s, memory_ratio)	
-	for k in k_list:
-		std.append( float(format(np.std( q_func(k,s) ),'.2f') ) )
-	print("s value: " + str(s) + " and memory ratio: " + str(memory_ratio)) 
-	for k in k_list:
-		print("years of memory: " + str(i+3) + ", years to reach 0.9: " + str( round((i+3)*memory_ratio )) + ", k value: " + str(k) + ", std: " + str(std[i]) + ", q values for years of experience " + str(q_func(k,s)))
-		i += 1
-	print
+	
+# each agent has s value and memory limit value. We need to find a k value that lets q be greater than 0.8, and less than 0.9 with a memory ratio of 0.5 and 0.166
+

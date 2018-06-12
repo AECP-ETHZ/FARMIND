422a2589c8a64469b74ba0ab29e0745f7ed7d6ee
Kevin Keller <kevin.ew.keller@gmail.com>
2018-06-12 15:21:27 +0200

    moved model to subfolder

--- src/mathematical_programming/MPConnection.java
+++ src/mathematical_programming/MPConnection.java
@@ -20,25 +20,26 @@ public class MPConnection implements MP_Interface{
 	File file; 
 	
 	public MPConnection() {
-		file = new File("p_allowedStratPrePost.csv");					   // delete last time period's simulation file
-		if (file.exists()) {file.delete();}
-				
+		file = new File("model\\p_allowedStratPrePost.csv");					   // delete last time period's simulation file
+		if (file.exists()) {
+			System.out.println("Deleted old results file");
+			file.delete();}	
 	}
 
 	@Override
 	public void runModel() {
 		Runtime runtime = Runtime.getRuntime();								   // java runtime to run commands
 		
-		File f = new File("Grossmargin_P4,00.csv");							   // delete previous results file before new run
+		File f = new File("model\\Grossmargin_P4,00.csv");							   // delete previous results file before new run
 		f.delete();
 		
 		try {
 			String name = System.getProperty("os.name").toLowerCase();
 			if (name.startsWith("win") ){
-				runtime.exec("cmd /C" + "run_gams.bat");						   // actually run command
+				runtime.exec("cmd /C" + "run_gams.bat");						         // actually run command
 			}
 			if (name.startsWith("mac")) {
-				runtime.exec("/bin/bash -c ./run_gams_mac.command");				   // actually run command
+				runtime.exec("/bin/bash -c ./run_gams_mac.command");				     // actually run command
 			}
 			System.out.println("Starting gams model");
 			System.out.println("Waiting for gams results to be generated");
@@ -129,7 +130,7 @@ public class MPConnection implements MP_Interface{
 		
 		List<Activity> allPossibleActivities = reader.getActivityList();			   // generated activity list with ID and name 
 		
-		File f = new File("Grossmargin_P4,00.csv");							   // actual results file
+		File f = new File("model\\Grossmargin_P4,00.csv");							   // actual results file
 		while (!f.exists()) {try {
 			Thread.sleep(1000);												   // wait until simulation finishes running
 		} catch (InterruptedException e) {
@@ -137,7 +138,7 @@ public class MPConnection implements MP_Interface{
 		}}
 
 		try {
-			Buffer = new BufferedReader(new FileReader("Grossmargin_P4,00.csv"));
+			Buffer = new BufferedReader(new FileReader("model\\Grossmargin_P4,00.csv"));
 			
 			Line = Buffer.readLine();
 			while ((Line = Buffer.readLine()) != null) {                       

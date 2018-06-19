be8b35be17844cd07bad36cf111110764c9cc1f7
Kevin Keller <kevin.ew.keller@gmail.com>
2018-06-19 14:24:58 +0200

    updated test data

---
+++ src/testing/X.java
@@ -0,0 +1,14 @@
+package testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class X {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}

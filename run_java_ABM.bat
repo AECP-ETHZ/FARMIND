3f011c503c164d3d8c48c14fcadc64b777a9b33f
Kevin Keller <kevin.ew.keller@gmail.com>
2018-08-02 13:13:07 +0200

    comments on batch file with extra arguments

--- run_java_ABM.bat
+++ run_java_ABM.bat
@@ -1,3 +1,9 @@
 :: This command calls a java jar file
-java -jar abm.jar 5 WEEDCONTROL
+:: optional commands are 
+:: -uncertainty :: if set to 0, then ABM will not use the dissimilarity calculations during simulations
+:: -individual_learning :: if set to 0, then the ABM will not use individual learning for activities during simulation
+:: -social_learning :: if set to 0, then the ABM will not use social learning for activities during simulation
+:: -activity_preference :: if set to 0, then the ABM will not use activity preference during simulation
+
+java -jar abm.jar -year 5 -modelName WEEDCONTROL
 pause

3a2e53be6a209012e6533c7e0dca21e34a5ca79c
Kevin Keller <kevin.ew.keller@gmail.com>
2018-08-24 14:30:44 +0200

    updated output log for variable sized activity sets

--- src/logging/ABMActivityLog.java
+++ src/logging/ABMActivityLog.java
@@ -18,6 +18,9 @@ public class ABMActivityLog {
 	private List<Activity> currentActivity;									   // current activity of the agent
 	private List<String> possibleActivity;								       // set of possible activities by the agent
 	private List<Activity> MPSelectedActivity;								   // activity actually selected by the MP
+	
+	public static final int SELECTED_ACTIVITY_SET_PRINTING_SIZE = 2;
+	public static final int PREVIOUS_ACTIVITY_SET_PRINTING_SIZE = 2;
 
 	/** 
 	 * Constructor for the CSV Log
@@ -58,24 +61,64 @@ public class ABMActivityLog {
 		BufferedWriter bw = new BufferedWriter(fw);
 		PrintWriter writer = new PrintWriter(bw);
 		
+		String name = "year,name,";
+		
+		for(int i = 0; i < PREVIOUS_ACTIVITY_SET_PRINTING_SIZE; i++) {
+			name = name + String.format("previous_activity_%s,",  i+1 );
+		}
+
+		for(int i = 0; i < SELECTED_ACTIVITY_SET_PRINTING_SIZE -1 ; i++) {
+			name = name + String.format("possible_activity_%s,",  i+1 );
+		}
+		
+		name = name + "strategy";
+		
 		if (file.length() == 0) {
-			writer.println("year,name,previous_activity,selected_activity,strategy");
+			writer.println(name);
 		}
 		
 		writer.print(String.format("%s,",this.year));
 		writer.print(String.format("%s,",this.getFarmId()));
 		
-		
-		for(int i = 0; i < this.currentActivity.size(); i++) {
-			writer.print(String.format("%s,",  this.currentActivity.get(i).getName()) );
+		// if PREVIOUS activity set is larger than printing limit, print NA for all options
+		if(this.currentActivity.size() == 0 || this.currentActivity.size() > PREVIOUS_ACTIVITY_SET_PRINTING_SIZE) {
+			for(int i = 0; i < PREVIOUS_ACTIVITY_SET_PRINTING_SIZE-1 ; i++) {
+				writer.print("NA," );
+			}
 		}
 		
-		for(int i = 0; i < this.MPSelectedActivity.size(); i++) {
-			writer.print(String.format("%s,",  this.MPSelectedActivity.get(i).getName()) );
+		// if PREVIOUS activity set is smaller than printing limit, print those activities plus NA if required
+		else {
+			for(int i = 0; i < PREVIOUS_ACTIVITY_SET_PRINTING_SIZE; i++) {
+				if (this.currentActivity.size() >= (i+1)) {
+					writer.print(String.format("%s,",  this.currentActivity.get(i).getName()) );
+				}
+				else {
+					writer.print("NA," );
+				}
+			}
+		}
+		
+		// if SELECTED activity set is larger than printing limit, print NA for all options
+		if(this.MPSelectedActivity.size() == 0 || this.MPSelectedActivity.size() > SELECTED_ACTIVITY_SET_PRINTING_SIZE) {
+			for(int i = 0; i < SELECTED_ACTIVITY_SET_PRINTING_SIZE-1 ; i++) {
+				writer.print("NA," );
+			}
+		}
+		
+		// if SELECTED activity set is smaller than printing limit, print those activities plus NA if required
+		else {
+			for(int i = 0; i < SELECTED_ACTIVITY_SET_PRINTING_SIZE; i++) {
+				if (this.MPSelectedActivity.size() >= (i+1)) {
+					writer.print(String.format("%s,",  this.MPSelectedActivity.get(i).getName()) );
+				}
+				else {
+					writer.print("NA," );
+				}
+			}
 		}
 		
 		writer.print(String.format("%s",this.strategy) );
-		
 		
 		writer.println("");
 		writer.close();

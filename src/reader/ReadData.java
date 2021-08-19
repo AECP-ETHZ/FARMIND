87db517739921c740e8b500c0a24b4b43300edbc
schmide <emanuel.schmid@id.ethz.ch>
2021-08-19 15:04:01 +0200

    unused import

--- src/reader/ReadData.java
+++ src/reader/ReadData.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.NumberFormatException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;

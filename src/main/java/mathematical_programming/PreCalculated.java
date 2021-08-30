58bd744c0d333b46c8be586bbbc563ea00c9d9ee
schmide <emanuel.schmid@id.ethz.ch>
2021-08-31 01:26:08 +0200

    precalculated MPI: change logic - incomes and activities - income and impact

--- src/main/java/mathematical_programming/PreCalculated.java
+++ src/main/java/mathematical_programming/PreCalculated.java
@@ -7,10 +7,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Properties;
+import java.util.Set;
 import java.util.logging.Logger;
 
 import com.opencsv.CSVReader;
@@ -28,9 +30,13 @@ import agent.Farm;
 public class PreCalculated implements MP_Interface{
 	private static final Logger LOGGER = Logger.getLogger("FARMIND_LOGGING");
 	
-    private Map<String,List<String>> possibleActivitiesState;
+    private Map<String,Set<String>> possibleActivitiesState;
     
-    private Map<String, List<Entry<String, Double>>> yearsHarvest;
+    private Map<String, List<Entry<String, Score>>> yearsHarvest;
+    
+    private Map<String, List<String>> incomeActivityMap;
+    
+    private final static String MP_DATA_FOLDER = "MPdata";
 	
     /**
      * Constructor
@@ -38,15 +44,38 @@ public class PreCalculated implements MP_Interface{
      * @param cmd : properties
      * @param simYear : MP_Interface conventional argument, unused
      * @param memoryLengthAverage : MP_Interface conventional argument, unused
+     * @throws IOException 
+     * @throws FileNotFoundException 
      */
-	public PreCalculated(Properties cmd, int simYear, int memoryLengthAverage) {
-	    this.possibleActivitiesState = new HashMap<String,List<String>>();
-	    this.yearsHarvest = new HashMap<String,List<Entry<String,Double>>>();
+	public PreCalculated(Properties cmd, int simYear, int memoryLengthAverage) throws FileNotFoundException, IOException {
+	    this.possibleActivitiesState = new HashMap<String,Set<String>>();
+	    this.yearsHarvest = new HashMap<String,List<Entry<String,Score>>>();
+	    
+	    this.incomeActivityMap = new HashMap<String,List<String>>();
+	    try (CSVReader reader = new CSVReader(new FileReader(
+	            String.format("%s/%s/transformation-activity-income.csv", cmd.get("data_folder"), PreCalculated.MP_DATA_FOLDER)
+	        ), ',')) {
+            String[] columns = reader.readNext();
+            
+            String[] row;
+            while ((row = reader.readNext()) != null) {
+                
+                String income = row[0].trim();
+                List<String> activities = new ArrayList<String>();
+                for (int i=1; i<columns.length; i++) {
+                    if (row[i].trim() == "1") {
+                        activities.add(columns[i].trim());
+                    }
+                }
+                
+                this.incomeActivityMap.put(income, activities);
+            }
+        }
 	}
 	
     @Override
     public void inputsforMP(Farm farm, List<String> possibleActivity) throws IOException {
-        this.possibleActivitiesState.put(farm.getFarmName(), new ArrayList<String>());
+        this.possibleActivitiesState.put(farm.getFarmName(), new HashSet<String>());
         this.possibleActivitiesState.get(farm.getFarmName()).addAll(possibleActivity);
     }
 
@@ -56,47 +85,91 @@ public class PreCalculated implements MP_Interface{
                 + "\n      nFarms: "+nFarms
                 + "\n      pricingAverage: "+pricingAverage
                 + "\n      memoryLengthAverage: "+memoryLengthAverage);
-	    // TODO : finalize file name and directory structure conventions
 	    try (CSVReader reader = new CSVReader(new FileReader(
-	        String.format("%s/precalc/Run_%d.csv", cmd.get("data_folder"), year)
+	        String.format("%s/MPdata/run%d.csv", cmd.get("data_folder"), PreCalculated.MP_DATA_FOLDER, year)
 	    ), ',')) {
-    	    String[] columns = reader.readNext();
-    	    
-    	    String[] row;
+	        String[] incomes = reader.readNext();
+            Map<String,Map<String,Score>> allFarmScores = new HashMap<String,Map<String,Score>>();
+            
+            // incomes
+    	    Map<String,Map<String,Double>> farmIncomes = new HashMap<String,Map<String,Double>>();
+            String[] row;
     	    while ((row = reader.readNext()) != null) {
     	        
                 String farmName = row[0].trim();
-                Map<String,Double> evaluation = new HashMap<String,Double>();
-                List<String> possibleActivities = this.possibleActivitiesState.get(farmName);
-                for (int i=1; i<columns.length; i++) {
-                    if (possibleActivities.contains(columns[i])) {
-                        evaluation.put(columns[i], Double.parseDouble(row[i]));
+                if (farmName == "") break;
+                
+                Map<String,Double> farmIncome = new HashMap<String,Double>();
+                for (int i=1; i<incomes.length; i++) {
+                    farmIncome.put(incomes[i], Double.parseDouble(row[i]));
+                }
+                farmIncomes.put(farmName, farmIncome);
+    	    }
+    	    
+    	    @SuppressWarnings("unused")
+            String[] climateLoad = reader.readNext(); // unnecessary: the row with ",THG1,THG2,..."
+    	        // is just there to separate incomes from climate effect
+    	    
+            // impact
+            while ((row = reader.readNext()) != null) {
+                
+                String farmName = row[0].trim();
+                if (farmName == "") break;
+                
+                Map<String,Score> farmScore = new HashMap<String,Score>();
+                for (int i=1; i<incomes.length; i++) {
+                    if (this.acceptableIncome(farmName, incomes[i])) {
+                    farmScore.put(incomes[i], new Score(
+                        farmIncomes.get(farmName).get(incomes[i]),
+                        Double.parseDouble(row[i])
+                    ));
                     }
                 }
                 
-                List<Entry<String, Double>> entryList = new ArrayList<Entry<String, Double>>(evaluation.entrySet());
-                Collections.sort(entryList, new Comparator<Entry<String, Double>>(){
+                // now we now everything about that farm
+                allFarmScores.put(farmName, farmScore);
+            }
+            
+            for (Entry<String, Map<String, Score>> farmScores : allFarmScores.entrySet()) {
+                List<Entry<String, Score>> entryList = new ArrayList<Entry<String, Score>>(farmScores.getValue().entrySet());
+                Collections.sort(entryList, new Comparator<Entry<String, Score>>(){
                     @Override
-                    public int compare(Entry<String, Double> a, Entry<String, Double> b) {
-                        double da = a.getValue() == null ? Double.NEGATIVE_INFINITY : a.getValue();
-                        double db = b.getValue() == null ? Double.NEGATIVE_INFINITY : b.getValue();
-                        return da > db ? -1 : da == db ? 0 : 1;
-                    }
+                    public int compare(Entry<String, Score> a, Entry<String, Score> b) {
+                        Score sa = a.getValue();
+                        Score sb = b.getValue();
+                        
+                        if (sa.income > sb.income) return -1;
+                        if (sa.income < sb.income) return 1;
+                        
+                        if (sa.impact < sb.impact) return -1;
+                        if (sa.impact > sb.impact) return 1;
+                        
+                        return 0;
+                   }
                 });
                 
-                this.yearsHarvest.put(farmName, entryList);
+                this.yearsHarvest.put(farmScores.getKey(), entryList);
             }
 	    }
 	}
 
+	private boolean acceptableIncome(String farmName, String income) {
+	    Set<String> possibleActivities = this.possibleActivitiesState.get(farmName);
+	    List<String> incomeActivities = this.incomeActivityMap.get(income);
+	    for (String activity : incomeActivities) {
+	        if (!possibleActivities.contains(activity)) return false;
+	    }
+	    return true;
+	}
+	
 	@Override
 	public List<Double> readMPIncomes(Properties cmd, List<Farm> allFarms) throws FileNotFoundException, IOException {
 		List<Double> incomes = new ArrayList<Double>();  // list of all farm incomes, same order as allFarms
 
 		for (Farm farm : allFarms) {
-		    List<Entry<String, Double>> possibleIncomes = this.yearsHarvest.get(farm.getFarmName());
-		    Double income = possibleIncomes.isEmpty() ? 0.0 : possibleIncomes.get(0).getValue();
-		    if (possibleIncomes.isEmpty()) {
+		    List<Entry<String, Score>> possibleScores = this.yearsHarvest.get(farm.getFarmName());
+		    Double income = possibleScores.isEmpty() ? 0.0 : possibleScores.get(0).getValue().income;
+		    if (possibleScores.isEmpty()) {
 		        LOGGER.info(String.format("n: %s, a: %s, i: %d",
 	                farm.getFarmName(),
 	                farm.getCurrentActivity().get(0).getName(),
@@ -114,16 +187,24 @@ public class PreCalculated implements MP_Interface{
 		List<ArrayList<Activity>> activities = new ArrayList<ArrayList<Activity>>();  // list of all farm activities selected by MP model
         
 		for (Farm farm : allFarms) {
-            List<Entry<String, Double>> topEntry = this.yearsHarvest.get(farm.getFarmName());
-            String topActivity = topEntry.isEmpty() ? null : topEntry.get(0).getKey();
             ArrayList<Activity> farmActivities = new ArrayList<Activity>();
-            if (topActivity != null) {
-                // based on the convention that activity names match this regular expression:
-                // /activity\d+/
-                // TODO: this convention should be optional. To abandon it, one could store a
-                //       dynamically generated map with activity names and ids somewhere.
-                int taId = Integer.parseInt(topActivity.substring(8));
-                farmActivities.add(new Activity(taId, topActivity));
+            
+            // that's not really a for loop, it just processes the first item - if any - and breaks.
+            // (another way to say: "if the list is not empty, get its first item and do something with it")
+            for (Entry<String, Score> topEntry : this.yearsHarvest.get(farm.getFarmName())) {
+            
+                String topScoreIncome = topEntry.getKey();
+                
+                for (String farmActivity : this.incomeActivityMap.get(topScoreIncome)) {
+                    // based on the convention that activity names match this regular expression:
+                    // /activity\d+/
+                    // TODO: this convention should be foramalized or optional. To abandon it, one could store a
+                    //       dynamically generated map with activity names and ids somewhere.
+                    farmActivities.add(new Activity(Integer.parseInt(farmActivity.substring(8)), farmActivity));
+                }
+                
+                // as stated above - not really a for loop.
+                break;
             }
             activities.add(farmActivities);
         }
@@ -136,7 +217,7 @@ public class PreCalculated implements MP_Interface{
 		// if the agent decides to opt-out we use this to return the correct opt-out activity
 		ArrayList<Activity> activities = new ArrayList<Activity>();
 		
-		Activity exit = new Activity(0,"exit");
+		Activity exit = new Activity(1,"activity01");
 		activities.add(exit);
 	
 		return activities;
---
+++ src/main/java/mathematical_programming/Score.java
@@ -0,0 +1,18 @@
+package mathematical_programming;
+
+/** 
+ * A primitive data class. 
+ * 
+ * @author schmide
+ *
+ */
+
+class Score {
+    public final Double income;
+    public final Double impact;
+    public Score(final Double income, final Double impact) {
+        this.income = income;
+        this.impact = impact;
+    }
+}
+    

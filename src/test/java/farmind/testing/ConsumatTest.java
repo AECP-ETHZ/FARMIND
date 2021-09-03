67797dc0b4d6c5d9d4b05fc59613759f8e772588
schmide <emanuel.schmid@id.ethz.ch>
2021-09-03 15:22:39 +0200

    improve Consumat.testMain

--- src/test/java/farmind/testing/ConsumatTest.java
+++ src/test/java/farmind/testing/ConsumatTest.java
@@ -1,29 +1,75 @@
 package farmind.testing;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
+
+import com.opencsv.CSVReader;
 
 import farmind.main.Consumat;
 
 public class ConsumatTest {
 
-    @Before
-    public void setUp() throws Exception {
-    }
-
-    @After
-    public void tearDown() throws Exception {
-    }
-
+    @SuppressWarnings("static-method")
     @Test
     public void testMain() throws FileNotFoundException, IOException {
-        Consumat.main(new String[] {"2", "test-out"});
+        int yers = 2;
+        Consumat.main(new String[] {""+yers, "test-out"});
+        
+        ArrayList<HashMap<String, HashMap<String, String>>> activityPrice 
+            = new ArrayList<HashMap<String, HashMap<String, String>>>();
+        ArrayList<HashMap<String, HashMap<String, String>>> parameters 
+            = new ArrayList<HashMap<String, HashMap<String, String>>>();
+        for (int i=0; i<yers; i++) {
+            activityPrice.add(new HashMap<String, HashMap<String, String>>());
+            parameters.add(new HashMap<String, HashMap<String, String>>());
+        }
+        
+        try (CSVReader reader = new CSVReader(new FileReader(
+                String.format("output/test-out_0_activity_actualPrice.csv")
+            ), ',')) {
+            String[] columns = reader.readNext();
+            
+            String[] row;
+            while ((row = reader.readNext()) != null) {
+                
+                int year = Integer.parseInt(row[0])-1;
+                String farm = row[1].trim();
+                
+                activityPrice.get(year).put(farm, new HashMap<String,String>());
+                for (int i=2; i<columns.length; i++) {
+                    activityPrice.get(year).get(farm).put(columns[i], row[i]);
+                }
+                
+            }
+        }
+        
+        try (CSVReader reader = new CSVReader(new FileReader(
+                String.format("output/test-out_0_parameters.csv")
+            ), ',')) {
+            String[] columns = reader.readNext();
+            
+            String[] row;
+            while ((row = reader.readNext()) != null) {
+                
+                int year = Integer.parseInt(row[0])-1;
+                String farm = row[1].trim();
+                
+                parameters.get(year).put(farm, new HashMap<String,String>());
+                for (int i=2; i<columns.length; i++) {
+                    parameters.get(year).get(farm).put(columns[i], row[i]);
+                }
+                
+            }
+        }
+        
+        assertEquals(activityPrice.get(yers-1).get("agent05").get("income"), "247861.0");
+        assertEquals(parameters.get(yers-1).get("agent05").get("possible_activity_e"), "activity05");
     }
-
 }
